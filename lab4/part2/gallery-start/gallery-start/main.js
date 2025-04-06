602d15b424e68a58c53b1b184d79e07f77020f67
Manthan420 <jayswalmanthan19@gmail.com>
2025-04-07 02:34:15 +0530

    Add darken/lighten toggle
    Added a toggle button to switch between dark and light overlay.

--- lab4/part2/gallery-start/gallery-start/main.js
+++ lab4/part2/gallery-start/gallery-start/main.js
@@ -28,3 +28,18 @@ newImage.addEventListener('click', function () {
 });
 });
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', function () {
+  const currentClass = btn.getAttribute('class');
+
+  if (currentClass === 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  } else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)';
+  }
+});
+
